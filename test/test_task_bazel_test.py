df7aab3517b01a761963dd4520b99aa764b29cc7
Mickael Gaillard <mick.gaillard@gmail.com>
2018-08-20 01:21:57 +0200

    test more task
    Signed-off-by: Mickael Gaillard <mick.gaillard@gmail.com>

--- test/test_task_bazel_test.py
+++ test/test_task_bazel_test.py
@@ -2,23 +2,46 @@
 # Licensed under the Apache License, Version 2.0
 
 
+import asyncio
 import pytest
 
 from colcon_bazel.task.bazel.test import BazelTestTask
 from colcon_core.package_descriptor import PackageDescriptor
 from colcon_core.task import TaskContext
+from colcon_core.verb.test import TestPackageArguments
 
+from pathlib import Path
 from tempfile import TemporaryDirectory
+
+
+class MockArgs(object):
+    def __init__(self, basepath):  # noqa: D107
+        super().__init__()
+        self.build_base = (basepath + '/build/')
+        self.install_base = (basepath + '/install/')
+        self.merge_install = (basepath + '/merge/')
+        self.test_result_base = (basepath + '/test/')
 
 
 @pytest.mark.asyncio
 async def test_task_test():
     with TemporaryDirectory(prefix='test_colcon_') as basepath:
         extension = BazelTestTask()
+
         desc = PackageDescriptor(basepath)
-        context = TaskContext(pkg=desc, args=[''], dependencies=set())
+        desc.name = "test"
+
+        args_verb = MockArgs(basepath)
+        args_pkg = TestPackageArguments(desc, args_verb)
+        args_pkg.path = basepath
+        args_pkg.build_base = args_verb.build_base
+        args_pkg.install_base = args_verb.install_base
+        args_pkg.merge_install = args_verb.merge_install
+        args_pkg.test_result_base = args_verb.test_result_base
+
+        context = TaskContext(pkg=desc, args=args_pkg, dependencies=set())
 
         extension.set_context(context=context)
-        rc = await extension.test()
+        ret = await extension.test()
 
-        assert rc == 0
+        assert ret
